ac96545e25b1dafcfa454b0d633e9e74828bbb4c
palloc <palloc.mknet10@gmail.com>
2017-06-11 00:07:22 +0900

    add gpu_name

--- client/lib/gpuinfo.py
+++ client/lib/gpuinfo.py
@@ -2,13 +2,15 @@
 import subprocess
 
 
+
+
 command = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 output = command.communicate()
 output_temp = output[0].decode('UTF-8').split('\n')
 output_list = []
-output_dict = {}
 
+gpu_name = []
 total_memory = []
 free_memory = []
 
@@ -16,12 +18,15 @@ for output_str in output_temp:
     output_list.append(output_str.replace(" ", ""))
     
 for i in range(len(output_list)):
-    try:
-        if output_list[i] == "FBMemoryUsage":
-            total_memory.append(output_list[i+1])
-            free_memory.append(output_list[i+3])
-    except:
-        pass
-        
-print(total_memory)
-        
+    if "ProductName" in output_list[i]:
+        gpu_name.append(output_list[i][output_list[i].find(":")+1:])
+            
+    elif "FBMemoryUsage" in output_list[i]:
+        total_memory.append(output_list[i+1])
+        free_memory.append(output_list[i+3])
+
+# for debug        
+for i in range(len(gpu_name)):
+    print(gpu_name[i])
+    print(total_memory[i])
+    print(free_memory[i])
