f82bec6038726f5b691f4f1addb4246199c970e4
palloc <palloc.mknet10@gmail.com>
2017-06-18 22:13:56 +0900

    add data

--- server/src/components/Top.vue
+++ server/src/components/Top.vue
@@ -1,38 +1,38 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1>GPU Resource</h1>
+    <div class="contents">{{ msg }}</div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+var $ = require('jquery')
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'container',
+    data () {
+	var gpu_resource = [
+	    {
+		number: '',
+		name: '',
+		total_memory: '',
+		free_memory: '',
+		utilization_rate: ''
+	    }
+	]
+	return {
+	    msg: 'hahaha'
+	}
     }
-  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+    margin: 3%;
+}
+
 h1, h2 {
   font-weight: normal;
 }
