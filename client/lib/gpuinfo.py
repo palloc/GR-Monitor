3fe0de522338f7066bedfb64202f686e119098ed
palloc <palloc.mknet10@gmail.com>
2017-06-22 15:12:28 +0900

    follow pep8

--- client/lib/gpuinfo.py
+++ client/lib/gpuinfo.py
@@ -7,7 +7,9 @@ class GPUInfo:
         self.output_list = []
 
     def get_gpu_info(self):
-        command = subprocess.Popen(['nvidia-smi', '-q'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        command = subprocess.Popen(['nvidia-smi', '-q'],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
         # reshape return data
         output = command.communicate()
         output_temp = output[0].decode('UTF-8').split('\n')
@@ -18,19 +20,26 @@ class GPUInfo:
         total_memory = []
         free_memory = []
         gpu_util = []
-    
+
         for i in range(len(self.output_list)):
             if "ProductName" in self.output_list[i]:
-                gpu_name.append(self.output_list[i][self.output_list[i].find(":")+1:])
-            
+                name_start = self.output_list[i].find(":") + 1
+                gpu_name.append(self.output_list[i][name_start:])
+
             elif "FBMemoryUsage" in self.output_list[i]:
-                total_memory.append(self.output_list[i+1][self.output_list[i+1].find(":")+1:self.output_list[i+1].find("MiB")])
-                free_memory.append(self.output_list[i+3][self.output_list[i+3].find(":")+1:self.output_list[i+3].find("MiB")])
+                total_start = self.output_list[i+1].find(":") + 1
+                total_end = self.output_list[i+1].find("MiB")
+                free_start = self.output_list[i+3].find(":") + 1
+                free_end = self.output_list[i+3].find("MiB")
+                total_memory.append(self.output_list[i+1][total_start:total_end])
+                free_memory.append(self.output_list[i+3][free_start:free_end])
 
             elif "Utilization" in self.output_list[i]:
-                gpu_util.append(self.output_list[i+1][self.output_list[i+1].find(":")+1:self.output_list[i+1].find("%")])
+                util_start = self.output_list[i+1].find(":") + 1
+                util_end = self.output_list[i+1].find("%")
+                gpu_util.append(self.output_list[i+1][util_start:util_end])
 
-        gpu_info = [{}]
+        gpu_info = []
 
         # create json shape
         for i in range(len(gpu_name)):
@@ -39,9 +48,7 @@ class GPUInfo:
             gpu.update({"name": gpu_name[i]})
             gpu.update({"total_memory": int(total_memory[i])})
             gpu.update({"free_memory": int(free_memory[i])})
-            gpu.update({"utilization_rate": int(gpu_util[i])})            
+            gpu.update({"utilization_rate": int(gpu_util[i])})
             gpu_info.append(gpu)
 
         return gpu_info
-
-        
